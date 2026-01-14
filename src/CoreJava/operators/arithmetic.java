1b3bfaef0f3938531902a7f7e5f5590b86bbba76
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2026-01-14 23:54:39 +0530

    operators

---
+++ src/CoreJava/operators/arithmetic.java
@@ -0,0 +1,13 @@
+package CoreJava.operators;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int p = 20;
+        int q = 10;
+
+        System.out.println("Addition: " + (p+q));
+        System.out.println("Subtraction: " + (p-q));
+        System.out.println("Multiplication: " + (p*q));
+        System.out.println("Division: " + (p/q));
+    }
+}
---
+++ src/CoreJava/operators/relational.java
@@ -0,0 +1,17 @@
+package CoreJava.operators;
+
+public class relational {
+    public static void main(String[] args) {
+        int p = 25;
+        int q = 78;
+
+        System.out.println(p == q);
+        System.out.println(p != q);
+        System.out.println(p<= q);
+        System.out.println(p >= q);
+        System.out.println(p < q);
+        System.out.println(p > q);
+
+
+    }
+}
